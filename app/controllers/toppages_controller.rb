2aca5d371840f7819ce2a7cc2f03775b17ad3739
KKTTOO <hongtaishankou755@gmail.com>
2021-08-19 21:37:54 +0000

    課題提出

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,22 +1,25 @@
 class ToppagesController < ApplicationController
   before_action :require_user_logged_in
-  before_action :kariitem
+  #before_action :kariitem
   
   def index
     if logged_in?
-      @item = current_user.items.build 
+      @item = current_user.items.build
       
-      @items = current_user.items.sort_by {|item| item.zanbi.to_i}
-      #.order(id: :desc)
+      #modelに記載した方が良さそう
+      @items = current_user.items.order(id: :desc).each{|i| i.generate_zanbi }.sort_by{|item| item.zanbi.to_i}
+      #@items = items.each{|i| i.generate_zanbi }.sort_by{|item| item.zanbi.to_i}
+      #sort_by {|item| item.zanbi.to_i}
+      #
     end
   end
   
   private
   
-  def kariitem
-    current_user.items.each do |item|
-      item.generate_zanbi
-    end
-  end
+  #def kariitem
+    #current_user.items.each do |item|
+      #item.generate_zanbi
+    #end
+  #end
   
 end
