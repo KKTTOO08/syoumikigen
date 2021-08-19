57571ff489393a573c30e0448711f0742204c8e2
KKTTOO <hongtaishankou755@gmail.com>
2021-08-19 22:03:17 +0000

    課題提出3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
-  before_action :kariitem
   
   def create
     @item = current_user.items.build(item_params)
@@ -9,7 +8,7 @@ class ItemsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_path
     else
-      @items = current_user.items.sort_by {|item| item.zanbi.to_i}
+      @items = current_user.items.order(id: :desc).each{|i| i.generate_zanbi }.sort_by{|item| item.zanbi.to_i}
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
@@ -31,12 +30,6 @@ class ItemsController < ApplicationController
     @item = current_user.items.find_by(id: params[:id])
     unless @item
       redirect_to root_path
-    end
-  end
-  
-  def kariitem
-    current_user.items.each do |item|
-      item.generate_zanbi
     end
   end
   
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,25 +1,12 @@
 class ToppagesController < ApplicationController
   before_action :require_user_logged_in
-  #before_action :kariitem
   
   def index
     if logged_in?
       @item = current_user.items.build
       
-      #modelに記載した方が良さそう
       @items = current_user.items.order(id: :desc).each{|i| i.generate_zanbi }.sort_by{|item| item.zanbi.to_i}
-      #@items = items.each{|i| i.generate_zanbi }.sort_by{|item| item.zanbi.to_i}
-      #sort_by {|item| item.zanbi.to_i}
-      #
     end
   end
-  
-  private
-  
-  #def kariitem
-    #current_user.items.each do |item|
-      #item.generate_zanbi
-    #end
-  #end
   
 end
